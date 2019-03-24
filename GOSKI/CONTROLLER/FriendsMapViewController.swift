8fc422ee4363c700be8ad7df377639fd70631251
hhr <honghu_haoran@163.com>
2019-03-24 15:27:17 -0400

    annotation should working correctly after deselecting friends

--- GOSKI/CONTROLLER/FriendsMapViewController.swift
+++ GOSKI/CONTROLLER/FriendsMapViewController.swift
@@ -13,7 +13,8 @@ import MapKit
 var internalFriendsVariable = [FriendItem]()
 var internalUserEmail = ""
 
-class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
+class FriendsMapViewController: UIViewController,CLLocationManagerDelegate{
+    
 
     var userEmail = ""{
         didSet{
@@ -63,32 +64,64 @@ class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
             
         }
     }
-        func retrieveFriendsLocations( fEmail : String){
-            var friendSharingLocation = false
-    //        for fEmail in self.friendsIWant {
-                let friendDB = Database.database().reference().child("USERS").child("\(fEmail.prefix(fEmail.count-4))")
-                friendDB.child("shareLocation").observe(.value) { (shareLocation) in
-                    print("\(fEmail): shareLocation: ",shareLocation)
-                    friendSharingLocation = shareLocation.value as! Bool
-                    if friendSharingLocation == true {
-                        print("true!!!")
-                        friendDB.child("location").observe(.value) { (location) in
-                            print("\(fEmail)location: ",location)
-                            /* plot this coordinate on the map!!*/
-                        }
-                }
-            
+    func retrieveFriendsLocations( fEmail : String){
+        var friendSharingLocation = false
+//        for fEmail in self.friendsIWant {
+            let friendDB = Database.database().reference().child("USERS").child("\(fEmail.prefix(fEmail.count-4))")
+            friendDB.child("shareLocation").observe(.value) { (shareLocation) in
+                print("\(fEmail): shareLocation: ",shareLocation)
+                friendSharingLocation = shareLocation.value as! Bool
                 
+            if friendSharingLocation == true {
+                print("true!!!")
+                friendDB.child("location").observe(.value) { (location) in
+                    let coordinates = location.value as! [String:CLLocationDegrees]
+                    print("\(fEmail)location: ",coordinates["longitude"]!,coordinates["latitude"]!)
+                    /* plot this coordinate on the map!!*/
+                    for annot in self.myMap.annotations{
+                        if annot.title == fEmail{
+                            self.myMap.removeAnnotation(annot)
+                        }
+                    }
+                    self.plotFriendsOnMap(email:fEmail,longitude: coordinates["longitude"]!,latitude: coordinates["latitude"]!)
+                    
+                    print("annotation count => ",self.myMap.annotations.count)
+                }
+            }else{
+                for annot in self.myMap.annotations{
+                    if annot.title == fEmail{
+                        self.myMap.removeAnnotation(annot)
+                    }
+                } 
             }
+        
+            
+        }
             
     //        }
     //        let friendDB = Database.database().reference().child("USERS").child("c@c")
     //        friendDB.child("shareLocation").observe(.value) { (DataSnapshot) in
     //            print("shareLocation: ",DataSnapshot)
     //        }
-        }
+    }
     
+    func plotFriendsOnMap(email:String,longitude:CLLocationDegrees,latitude:CLLocationDegrees){
+//        for friendItem in internalFriendsVariable{
+//            if friendItem.selected == true {
+//                //check whether this selected friend is sharing location
+//            }
+//        }
+        let tmpAnnot = MKPointAnnotation()
+        tmpAnnot.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        tmpAnnot.title = "\(email)"
+//        tmpAnnot
+        self.myMap.addAnnotation(tmpAnnot)
+        
+    }
+
     override func viewDidAppear(_ animated: Bool) {
+        print("----veiwDidAppear")
+        self.myMap.removeAnnotations(self.myMap.annotations)
         self.friendsIWant = [String]()
         retrieveFriendsIWant()
 //        print("=============================")
@@ -101,6 +134,7 @@ class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("----veiwDidload")
         self.userEmail = Auth.auth().currentUser!.email!
         Database.database().reference().child("USERS").child("\(userEmail.prefix(userEmail.count-4))").updateChildValues(["shareLocation":true])
         /*get friends from DB*/
@@ -161,12 +195,12 @@ class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        sup
-        
-            print("yyyyyyyyes")
-        
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+////        sup
+//
+//            print("yyyyyyyyes")
+//
+//    }
     
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -184,13 +218,7 @@ class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
 
         }
 
-        func plotFriendsOnMap(){
-            for friendItem in internalFriendsVariable{
-                if friendItem.selected == true {
-                    //check whether this selected friend is sharing location
-                }
-            }
-        }
+      
 
 
 
@@ -220,95 +248,10 @@ class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
 
 
 
-
-
-//import UIKit
-//import Firebase
-//import CoreLocation
-//import MapKit
-//
-//class FriendsMapViewController: UIViewController,CLLocationManagerDelegate {
-//
-//    var userEmail = internalUserEmail
-//
-//    var locationManager = CLLocationManager()
-//    var userLongitude : CLLocationDegrees = 0
-//    var userLatitude : CLLocationDegrees = 0
-//    @IBOutlet weak var myMap: MKMapView!
-//    var mapAlreadyCentered = false
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        print("friendsmapview:",internalFriendsVariable)
-//        // Do any additional setup after loading the view.
-//        /*myMap*/
-//        self.myMap.showsUserLocation = true
-//        /*setup coreLocationManager */
-//        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        self.locationManager.delegate = self
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.startUpdatingLocation()
-//        centerUser()
-//        prettyPrintFriendItem(friendItems: internalFriendsVariable)
-//
-//    }
-//    func prettyPrintFriendItem(friendItems : [FriendItem]){
-//        for item in friendItems{
-//            print("====>friendEmail:",item.friendEmail)
-//            print("selected:",item.selected)
-////            print("locationReady:",item.locationReady)
-////            print("location:",item.location)
-//        }
-//
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        let location = locations[locations.count - 1]
-//
-//        if location.horizontalAccuracy > 0{
-//            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
-//            Database.database().reference().child("USERS").child("\(userEmail.prefix(userEmail.count-4))").updateChildValues(["location":["longitude":location.coordinate.longitude,"latitude":location.coordinate.latitude]])
-//            self.userLatitude = location.coordinate.latitude
-//            self.userLongitude = location.coordinate.longitude
-//            if self.mapAlreadyCentered == false{
-//                centerUser()
-//                mapAlreadyCentered = true
-//            }
-//
-//        }
-//
-//        func plotFriendsOnMap(){
-//            for friendItem in internalFriendsVariable{
-//                if friendItem.selected == true {
-//                    //check whether this selected friend is sharing location
-//                }
-//            }
-//        }
-//
-//
-//
-//    }
-//
-//    @IBAction func centerButtonPressed(_ sender: UIBarButtonItem) {
-//        centerUser()
-//
-//    }
-//
-//    func centerUser(){
-//        let center = CLLocationCoordinate2D(latitude: self.userLatitude, longitude: self.userLongitude)
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-//        self.myMap.setRegion(region, animated: true)
-//    }
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
+//extension FriendsMapViewController : MKAnnotationView{
+////    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+////        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+////        clusteringIdentifier = "unicycle"
+////    }
 //
 //}
